12241c232216db92a26b191a94114463e55b334e
sarahend1122-creator <sarahend1122@gmail.com>
2026-03-01 17:27:45 +0200

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,8 +2,9 @@ import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compare } from "bcryptjs";
 import { prisma } from "./prisma";
-
+import { authOptions } from "../../../lib/auth";
 export const authOptions: NextAuthOptions = {
+  
   session: {
     strategy: "jwt",
   },
